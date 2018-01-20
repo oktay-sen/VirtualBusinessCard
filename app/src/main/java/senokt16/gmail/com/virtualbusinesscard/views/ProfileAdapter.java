f0f60772851a043d08b59fb8ba220a558b3bc4b9
iantai <ihbt2@cam.ac.uk>
2018-01-20 23:24:34 +0000

    commit before pull

---
+++ app/src/main/java/senokt16/gmail/com/virtualbusinesscard/views/ProfileAdapter.java
@@ -0,0 +1,10 @@
+package senokt16.gmail.com.virtualbusinesscard.views;
+
+import android.support.v7.widget.RecyclerView;
+
+/**
+ * Created by Ian_Tai on 2018-01-20.
+ */
+
+public class ProfileAdapter extends RecyclerView.Adapter {
+}
