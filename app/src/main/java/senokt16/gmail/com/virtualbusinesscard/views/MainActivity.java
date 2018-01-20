ab40f9f9d1df1a228dd7697b8bc47510cb9c6b25
Oktay Sen <senokt.16@gmail.com>
2018-01-20 16:48:45 +0000

    WIP Profile

--- app/src/main/java/senokt16/gmail/com/virtualbusinesscard/views/MainActivity.java
+++ app/src/main/java/senokt16/gmail/com/virtualbusinesscard/views/MainActivity.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
@@ -32,7 +33,8 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-
+        cardsView = findViewById(R.id.cards_view);
+        cardsView.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false));
         cardsView.setAdapter(new CardsAdapter());
     }
 
