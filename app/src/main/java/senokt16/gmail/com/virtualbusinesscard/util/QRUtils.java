74545b9f316318daaae0c1e630bbf16bf45f2c2a
MJHutchinson <hutchinson.michael.john@gmail.com>
2018-01-20 17:15:28 +0000

    Added QR generation functionality

---
+++ app/src/main/java/senokt16/gmail/com/virtualbusinesscard/util/QRUtils.java
@@ -0,0 +1,51 @@
+package senokt16.gmail.com.virtualbusinesscard.util;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.MultiFormatWriter;
+import com.google.zxing.WriterException;
+import com.google.zxing.common.BitMatrix;
+
+import senokt16.gmail.com.virtualbusinesscard.R;
+
+
+
+public class QRUtils {
+
+    Bitmap TextToImageEncode(Context context, String Value, int sizePixels) throws WriterException {
+        BitMatrix bitMatrix;
+        try {
+            bitMatrix = new MultiFormatWriter().encode(
+                    Value,
+                    BarcodeFormat.QR_CODE,
+                    sizePixels, sizePixels, null
+            );
+
+        } catch (IllegalArgumentException Illegalargumentexception) {
+
+            return null;
+        }
+        int bitMatrixWidth = bitMatrix.getWidth();
+
+        int bitMatrixHeight = bitMatrix.getHeight();
+
+        int[] pixels = new int[bitMatrixWidth * bitMatrixHeight];
+
+        for (int y = 0; y < bitMatrixHeight; y++) {
+            int offset = y * bitMatrixWidth;
+
+            for (int x = 0; x < bitMatrixWidth; x++) {
+
+                pixels[offset + x] = bitMatrix.get(x, y) ?
+                        context.getResources().getColor(R.color.QRBlack):context.getResources().getColor(R.color.QRWhite);
+            }
+        }
+        Bitmap bitmap = Bitmap.createBitmap(bitMatrixWidth, bitMatrixHeight, Bitmap.Config.ARGB_4444);
+
+        bitmap.setPixels(pixels, 0, sizePixels, 0, 0, bitMatrixWidth, bitMatrixHeight);
+        return bitmap;
+    }
+
+}
